81d23c44a65947bcf5c8a1b51df754d566c0d028
cogsincogs <jamiewthomas13@gmail.com>
2023-10-28 17:32:37 +0100

    Move model-drawing logic to a separate function

--- main.cpp
+++ main.cpp
@@ -10,40 +10,40 @@ const int height = 800;
 
 int main(int argc, char **argv)
 {
-    if (2 == argc)
-    {
-        model = new Model(argv[1]);
-    }
-    else
-    {
-        model = new Model("obj/head.obj");
-    }
+    void drawModel(TGAImage* image, Model* model);
+    
+    model = new Model("obj/head.obj");
     
     TGAImage image(width, height, TGAImage::RGB);
 
-    for (int i = 0; i < model->nfaces(); i++)
-    {
-        std::vector<int> face = model->face(i);
-        
-        for (int j = 0; j < 3; j++)
-        {
-            // These two vertices make up a line
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-
-            // This converts the line into the image space
-            int x0 = (v0.x + 1.0) * width/2.0;
-            int y0 = (v0.y + 1.0) * height/2.0; 
-            int x1 = (v1.x + 1.0) * width/2.0; 
-            int y1 = (v1.y + 1.0) * height/2.0; 
-
-            // Drawing the line
-            Line line = Line(x0, y0, x1, y1, &image, white);
-        }
-    }
+    drawModel(&image, model);
 
     image.flip_vertically(); // Origin should be left bottom corner
     image.write_tga_file("output.tga");
     delete model;
     return 0;
+}
+
+void drawModel(TGAImage* image, Model* model)
+{
+    for (int i = 0; i < model->nfaces(); i++)
+        {
+            std::vector<int> face = model->face(i);
+            
+            for (int j = 0; j < 3; j++)
+            {
+                // These two vertices make up a line
+                Vec3f v0 = model->vert(face[j]);
+                Vec3f v1 = model->vert(face[(j+1)%3]);
+
+                // This converts the line into the image space
+                int x0 = (v0.x + 1.0) * width/2.0;
+                int y0 = (v0.y + 1.0) * height/2.0; 
+                int x1 = (v1.x + 1.0) * width/2.0; 
+                int y1 = (v1.y + 1.0) * height/2.0; 
+
+                // Drawing the line
+                Line line = Line(x0, y0, x1, y1, image, white);
+            }
+        }
 }
