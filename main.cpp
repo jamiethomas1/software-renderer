b1f1ae8b0c64fdce486cab9fe7a7c37e37097dfb
cogsincogs <jamiewthomas13@gmail.com>
2023-10-28 12:20:54 +0100

    Clean up directory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "tgaimage.h"
-#include "model.h"
+#include "includes/tgaimage.h"
+#include "includes/model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
