60140a452462160d0e66acad65b1faf6bd04af24
cogsincogs <jamiewthomas13@gmail.com>
2023-10-29 15:03:22 +0000

    Implement Canvas::fillTriangle() method

--- canvas.cpp
+++ canvas.cpp
@@ -1,5 +1,8 @@
 #include "canvas.h"
 
+TGAColor tempred = TGAColor(255, 0, 0, 255);
+TGAColor tempgreen = TGAColor(0, 255, 0, 255);
+
 Canvas::Canvas(int width, int height, int bpp) : TGAImage(width, height, bpp)
 {
 }
@@ -47,7 +50,7 @@ void Canvas::drawLine(int x0, int y0, int x1, int y1, TGAColor &color)
          * derror (2 * dy) is added to error each loop. If error then becomes larger than dx,
          * increase/decrease y value by 1 (depending on which direction the line is in),
          * and account for that increase/decrease in the error variable.
-        */
+         */
         error += derror;
         if (error > dx)
         {
@@ -62,11 +65,77 @@ void Canvas::drawLine(Vec2i v0, Vec2i v1, TGAColor &color)
     this->drawLine(v0.x, v0.y, v1.x, v1.y, color);
 }
 
-void Canvas::drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAColor &color)
+void Canvas::drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAColor &color, bool fill)
 {
+    /**
+     * Sort vertices by increasing y-value. This is so that the line that goes top-to-bottom of the
+     * whole triangle is always the line t2-t0. This makes line-by-line filling easier.
+     */
+    if (t0.y > t1.y)
+        std::swap(t0, t1);
+    if (t0.y > t2.y)
+        std::swap(t0, t2);
+    if (t1.y > t2.y)
+        std::swap(t1, t2);
+
     this->drawLine(t0, t1, color);
     this->drawLine(t1, t2, color);
     this->drawLine(t2, t0, color);
+
+    if (fill)
+    {
+        this->fillTriangle(t0, t1, t2, color);
+    }
+}
+
+void Canvas::fillTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAColor &color)
+{
+    // Total triangle height, for calculating points along the total height of the triangle
+    int totalHeight = t2.y - t0.y;
+
+    // Bottom segment height for calculating points along the height of the bottom segment
+    int bottomSegmentHeight = t1.y - t0.y + 1;
+    for (int y = t0.y; y < t1.y; y++)
+    {
+        /**
+         * Normalised representation of how far along the triangle/segment height current y-value is
+         * Naming assumes left side is bottom-to-top of triangle, but can be different
+         */
+        float leftFraction = (float)(y - t0.y) / totalHeight;
+        float rightFraction = (float)(y - t0.y) / bottomSegmentHeight;
+
+        /**
+         * Get difference between top and bottom vertices of triangle/segment, multiply by relevant
+         * normalised fraction to find position along that line, then add to t0 to position
+         * that point on the image relative to the triangle
+         */
+        Vec2i leftPoint = t0 + (t2 - t0) * leftFraction;
+        Vec2i rightPoint = t0 + (t1 - t0) * rightFraction;
+
+        this->drawLine(leftPoint.x, y, rightPoint.x, y, color);
+    }
+
+    // Top segment height for calculating points along the height of the top segment
+    int topSegmentHeight = t2.y - t1.y;
+    for (int y = t1.y; y < t2.y; y++)
+    {
+        /**
+         * Normalised representation of how far along the triangle/segment height current y-value is
+         * Naming assumes left side is bottom-to-top of triangle, but can be different
+         */
+        float leftFraction = (float)(y - t0.y) / totalHeight;
+        float rightFraction = (float)(y - t1.y) / topSegmentHeight;
+
+        /**
+         * Get difference between top and bottom vertices of triangle/segment, multiply by relevant
+         * normalised fraction to find position along that line, then add to t0 to position
+         * that point on the image relative to the triangle
+         */
+        Vec2i leftPoint = t0 + (t2 - t0) * leftFraction;
+        Vec2i rightPoint = t1 + (t2 - t1) * rightFraction;
+
+        this->drawLine(leftPoint.x, y, rightPoint.x, y, color);
+    }
 }
 
 void Canvas::drawModel(Model *model, TGAColor &color)
--- canvas.h
+++ canvas.h
@@ -7,6 +7,7 @@
 
 class Canvas : public TGAImage {
     private:
+        void fillTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAColor &color);
 
     public:
         Canvas(int width, int height, int bpp = Canvas::RGB);
@@ -14,7 +15,7 @@ class Canvas : public TGAImage {
 
         void drawLine(int x0, int y0, int x1, int y1, TGAColor &color);
         void drawLine(Vec2i v0, Vec2i v1, TGAColor &color);
-        void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAColor &color);
+        void drawTriangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAColor &color, bool fill = true);
         void drawModel(Model *model, TGAColor &color);
 };
 
